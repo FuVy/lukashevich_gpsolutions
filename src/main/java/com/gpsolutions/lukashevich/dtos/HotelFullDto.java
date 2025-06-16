59eec0dc90b98e23801fbe805946a53bc75d73bd
Dzmitry <amigo1044@yandex.ru>
2025-06-16 03:26:12 +0300

    Update HotelFullDto.java

--- src/main/java/com/gpsolutions/lukashevich/dtos/HotelFullDto.java
+++ src/main/java/com/gpsolutions/lukashevich/dtos/HotelFullDto.java
@@ -5,6 +5,7 @@ import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.Value;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Value
@@ -30,5 +31,5 @@ public class HotelFullDto {
     List<@NotNull(message = "Amenity name can't be null.")
                 @NotEmpty(message = "Amenity name can't be empty.")
                 @NotBlank(message = "Amenity name can't be empty.")
-                String> amenities;
+                String> amenities = new ArrayList<>();
 }
