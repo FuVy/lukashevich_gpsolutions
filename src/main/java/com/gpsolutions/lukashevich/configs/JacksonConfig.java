3f4728ac2ee2bd65da66e26c166199e3c04128fd
Dzmitry <amigo1044@yandex.ru>
2025-06-18 02:10:34 +0300

    string deserializer
    refactored string deserializer into its own class

--- src/main/java/com/gpsolutions/lukashevich/configs/JacksonConfig.java
+++ src/main/java/com/gpsolutions/lukashevich/configs/JacksonConfig.java
@@ -1,8 +1,5 @@
 package com.gpsolutions.lukashevich.configs;
 
-import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.databind.DeserializationContext;
-import com.fasterxml.jackson.databind.deser.std.StringDeserializer;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,26 +7,9 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class JacksonConfig {
     @Bean
-    public SimpleModule stringDeserializerModule() {
+    public SimpleModule stringDeserializerModule(CustomStringDeserializer stringDeserializer) {
         final var module = new SimpleModule();
-
-        module.addDeserializer(String.class, new StringDeserializer() {
-            @Override
-            public String deserialize(JsonParser p, DeserializationContext ctx) throws java.io.IOException {
-                final var value = super.deserialize(p, ctx);
-                if (value == null) {
-                    return null;
-                }
-
-                final var trimmed = value.trim();
-                if (trimmed.isEmpty()) {
-                    return null;
-                }
-
-                return trimmed;
-            }
-        });
-
+        module.addDeserializer(String.class, stringDeserializer);
         return module;
     }
 }
